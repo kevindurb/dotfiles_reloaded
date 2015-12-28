299b6f188de1581b4c8e8d50c2c70f17a88f9669
Kevin Durbin <kdurbin@churchcommunitybuilder.com>
2015-12-28 15:36:13 -0700

    add tmux plugin manager

--- install.sh
+++ install.sh
@@ -58,6 +58,7 @@ ln -s ~/dotfiles/vimrc     .vimrc
 ln -s ~/dotfiles/vim       .vim
 ln -s ~/dotfiles/zsh       .zsh
 ln -s ~/dotfiles/tmux.conf .tmux.conf
+ln -s ~/dotfiles/tmux      .tmux
 
 cd $PREVIOUS_DIR
 echo "Dotfiles installed!"
