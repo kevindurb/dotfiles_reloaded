657733d2f99e30f449a2e2c0d8a06f40d6543e52
Kevin Durbin <kdurbin@churchcommunitybuilder.com>
2016-10-28 16:42:38 -0600

    more cool things

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -3,6 +3,8 @@ ZSH=$HOME/dotfiles/zsh
 # antigen
 source $ZSH/antigen/antigen.zsh
 
+antigen use oh-my-zsh
+
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle zsh-users/zsh-completions src
