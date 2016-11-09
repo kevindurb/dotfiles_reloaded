3b4dd12bac6d74ae4917af9745e3d292684d5635
Kevin Durbin <kevindurb@gmail.com>
2016-11-09 16:55:57 -0700

    CLEAN UP THE ERR THING

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,10 +7,8 @@ export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 export EDITOR='vim'
 
 # make it easier to get to my rc files
-export MYVIMRC=$HOME/.vimrc
+export MYVIMRC=$HOME/vim/.vimrc
 export MYZSHRC=$HOME/.zshrc
-export MYVIMKEYMAPS=$HOME/.vim/plugin/keymaps.vim
-export MYVIMSETTINGS=$HOME/.vim/plugin/settings.vim
 
-# nvm
-export NVM_DIR=~/.nvm
+# use local php at work for faster test running
+export USE_LOCAL_PHP=1
