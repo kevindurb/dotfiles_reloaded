32e704ca7eb064bb3341b6833106c7020660aeb9
Kevin Durbin <kevindurb@gmail.com>
2018-06-02 22:19:10 -0600

    do things

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,11 @@ if [[ $PLATFORM == 'Darwin' ]]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   echo "Installing Deps..."
-  brew install vim git tig ruby zsh cmake cmatrix
+  brew install vim git tig ruby zsh cmake cmatrix autojump ripgrep tmux yarn watchman autoenv mongodb
+  brew tap caskroom/fonts
+  brew cask install font-fira-code react-native-debugger
+  brew services start mongodb
+  brew install gnu-sed --with-default-names
 elif which apt-get > /dev/null; then
   echo "Determined linux with apt..."
   echo "Installing Deps..."
@@ -48,14 +52,20 @@ ln -s ~/dotfiles/gitignore      ~/.gitignore
 ln -s ~/dotfiles/zsh            ~/.zsh
 ln -s ~/dotfiles/vim            ~/.vim
 ln -s ~/dotfiles/tmux           ~/.tmux
-ln -s ~/dotfiles/zsh/init.zsh   ~/.zshrc
-ln -s ~/dotfiles/vim/init.vim   ~/.vimrc
-ln -s ~/dotfiles/tmux/init.conf ~/.tmux.conf
+ln -s ~/dotfiles/zsh/zshrc      ~/.zshrc
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/zprofile       ~/.zprofile
+ln -s ~/dotfiles/git/gitconfig  ~/.gitconfig
+
+echo "Creating vim backup dir..."
+mkdir ~/.vim/backup
 
 cd $PREVIOUS_DIR
 
 echo "Installing vim plugins..."
 vim +PlugInstall +qa
+
+echo "Installing italics terminfo"
+tic ~/dotfiles/xterm-256color-italic.terminfo
 
 echo "Dotfiles installed!"
