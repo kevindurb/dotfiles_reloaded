b62bde94b79dd2114b4c5ae1294fdd02118c3690
Kevin Durbin <kevindurb@gmail.com>
2018-08-31 15:12:29 -0600

    a great many things

--- scripts/clean_docker.sh
+++ scripts/clean_docker.sh
@@ -1,5 +1,37 @@
 # https://gist.github.com/bastman/5b57ddb3c11942094f8d0a97d461b430
-docker volume rm $(docker volume ls -qf dangling=true)
-docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
-docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
-docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+
+echo "Checking volumes..."
+VOLUMES=$(docker volume ls -qf dangling=true)
+if [[ $(echo "$VOLUMES" | wc -l) -ge 1 ]]
+then
+  echo "Deleting volumes..."
+  docker volume rm $VOLUMES
+  echo "Done!"
+fi
+
+echo "Checking networks..."
+NETWORKS=$(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+if [[ $(echo "$NETWORKS" | wc -l) -ge 1 ]]
+then
+  echo "Deleting networks..."
+  docker network rm $NETWORKS
+  echo "Done!"
+fi
+
+echo "Checking images..."
+IMAGES=$(docker images | grep "none" | awk '/ / { print $3 }')
+if [[ $(echo "$IMAGES" | wc -l) -ge 1 ]]
+then
+  echo "Deleting images..."
+  docker rmi $IMAGES
+  echo "Done!"
+fi
+
+echo "Checking containers..."
+CONTAINERS=$(docker ps -qa --no-trunc --filter "status=exited")
+if [[ $(echo "$CONTAINERS" | wc -l) -ge 1 ]]
+then
+  echo "Deleting containers..."
+  docker rm $CONTAINERS
+  echo "Done!"
+fi
--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -5,10 +5,9 @@ if [[ $PLATFORM == 'Darwin' ]]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   echo "Installing Deps..."
-  brew install vim git tig ruby zsh cmake cmatrix autojump ripgrep tmux yarn watchman autoenv mongodb coreutils
+  brew install vim git tig ruby zsh cmake cmatrix autojump ripgrep tmux yarn watchman autoenv mongodb coreutils prettyping bat ncdu htop
   brew tap caskroom/fonts
   brew cask install font-fira-code react-native-debugger
-  brew services start mongodb
   brew install gnu-sed --with-default-names
 else
   echo "OS not supported... exiting..."
